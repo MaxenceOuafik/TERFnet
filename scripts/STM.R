373fa5feee85c87a5241508abe03b577a2749337
Asrahal <maxence.ouafik@gmail.com>
2022-09-13 17:53:26 +0200

    STM

--- scripts/STM.R
+++ scripts/STM.R
@@ -1,1 +1,216 @@
+# Ce script vise à analyser le contenu des tweets via la technique de la STM (Structural Topic Models) 
+# pour déterminer quels sont les sujets qui reviennent le plus et quelles communautés tendent à s'exprimer de la sorte
+
 library(stm)
+library(tidytext)
+library(tidystopwords)
+library(Twitmo)
+library(furrr)
+library(ggthemes)
+
+
+# La première étape consiste simplement à récupérer les données adéquates pour ce travail. Pour ce faire, j'ai choisi
+# de n'inclure que les tweets dont l'utilisateur avait identifié dans une communauté (et donc pas les tweets
+# des gens qui gravitaient autour du réseau sans lien direct) et de ne pas inclure les retweets. 
+# En outre, les données ont été nettoyées de telle façon à ce que chaque ligne corresponde à un seul mot et les stopwords 
+# ont été supprimés
+
+tidy_swords <- generate_stoplist(language = "French",
+                                 output_form = 1)
+
+custom_swords <- c("planning",
+                   "planningfamilial", 
+                   "leplanning", 
+                   "familial", 
+                   "c'est", 
+                   "cest", 
+                   "être", 
+                   "fait", 
+                   "sait", 
+                   "quand",
+                   "plus",
+                   "peuvent",
+                   "rien",
+                   "non",
+                   "aussi",
+                   "alors",
+                   "peut",
+                   "parce",
+                   "comme",
+                   "cétait",
+                   "aton",
+                   "#planningfamilial",
+                   "nai",
+                   "quil",
+                   "jai",
+                   "va",
+                   "nest",
+                   "là",
+                   "na",
+                   "peu",
+                   "dit",
+                   "quun",
+                   "quon",
+                   "pf",
+                   "homme",
+                   "hommes",
+                   "faut",
+                   "pas",
+                   "même")
+
+
+remove_reg <- "&amp;|&lt;|&gt;"
+
+tidy_tweets <- planning_full%>% 
+  left_join(select(named_communities, Label, modularity_class, outdegree), by = c("screen_name" = "Label")) %>%
+  filter(!grepl("RT",full_text),
+         !is.na(modularity_class),
+         outdegree >= 1) %>%
+  mutate(full_text = str_remove_all(full_text, remove_reg),
+         tweet_id = row_number()) %>%
+  distinct(full_text, .keep_all = TRUE) %>%
+  unnest_tokens(word, full_text, token = "tweets") %>%
+  filter(!word %in% tidy_swords,
+         !word %in% custom_swords,
+         !word %in% str_remove_all(tidy_swords, "'"),
+         str_detect(word, "[a-z]"),
+         !grepl("https", word),
+         !grepl("@", word),
+         modularity_class != "Inclassable",
+         modularity_class != "Divers gauches") %>%
+ select(screen_name, word, modularity_class, tweet_id) %>%
+  rename(community = modularity_class,
+         user = screen_name)
+
+# On profite d'avoir des données propres pour faire un peu de data mining afin de voir quels sont les mots
+# les plus fréquents dans chaque communauté
+
+tidy_plot <- tidy_tweets %>%
+  group_by(community) %>%
+  count(word, sort = TRUE) %>%
+  top_n(5) %>%
+  mutate(word = reorder(word,n)) %>%
+  ggplot(aes(n, reorder_within(word, n, community), fill = community)) +
+  scale_y_reordered() +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(vars(community), scales = "free_y") 
+
+# Ensuite, la vraie modélisation des sujets commence. On débute par créer une matrice creuse avec le texte des tweets
+# Ainsi qu'un identifiant pour voir à quel tweet chaque mot appartient.
+
+tidy_sparse <- tidy_tweets %>%
+  count(tweet_id, community, word) %>%
+  cast_sparse(tweet_id, word, n)
+
+# Le paramètre le plus important est le nombre de sujets contenus dans la modélisation. Sans forte idée a priori, 
+# j'ai entraîné le modèle avec une plusieurs possibilités, de 5 à 20 sujets afin de déterminer les plus adéquats
+
+plan(multiprocess)
+models_evaluation <- data_frame(K = c(5, 10, 15, 20)) %>%
+  mutate(topic_model = future_map(K, ~ stm(tidy_sparse,
+                                           K = .,
+                                           init.type = "Spectral",
+                                           verbose = TRUE)))
+
+# On calcule ensuite certaines mesures qui permettent d'évaluer les modèles
+
+heldout <- make.heldout(tidy_sparse)
+
+k_result <- models_evaluation %>%
+  mutate(exclusivity = map(topic_model, exclusivity),
+         semantic_coherence = map(topic_model, semanticCoherence, tidy_sparse),
+         eval_heldout = map(topic_model, eval.heldout, heldout$missing),
+         residual = map(topic_model, checkResiduals, tidy_sparse),
+         bound =  map_dbl(topic_model, function(x) max(x$convergence$bound)),
+         lfact = map_dbl(topic_model, function(x) lfactorial(x$settings$dim$K)),
+         lbound = bound + lfact,
+         iterations = map_dbl(topic_model, function(x) length(x$convergence$bound)))
+
+# La visualisation de ces mesures permet d'estimer que le nombre de sujets idéaux se situe aux alentours de 5-10
+
+k_result %>%
+  transmute(K,
+            `Lower bound` = lbound,
+            Residuals = map_dbl(residual, "dispersion"),
+            `Semantic coherence` = map_dbl(semantic_coherence, mean),
+            `Held-out likelihood` = map_dbl(eval_heldout, "expected.heldout")) %>%
+  gather(Metric, Value, -K) %>%
+  ggplot(aes(K, Value, color = Metric)) +
+  geom_line(size = 1.5, alpha = 0.7, show.legend = FALSE) +
+  facet_wrap(~Metric, scales = "free_y") +
+  labs(x = "K (number of topics)",
+       y = NULL,
+       title = "Diagnostics du modèle, par nombre de sujets",
+       subtitle = "Le nombre adéquat de sujets semble être aux alentours de 5-10")
+
+# La cohérence sémantique est une mesure qui est plus élevée lorsque les mots les plus probables dans un sujet
+# sont souvent présents en même temps dans un document. Cependant, vu le faible nombre de sujets dans cette analyse, 
+# La valeur peut être artificiellement gonflée. Il est donc intéressant de regarder également l'exclusivité, qui 
+# est plus élevée lorsque les mots dans un sujet tendent à être exclusifs à ce sujet, et de trouver le meilleur compromis
+
+k_result %>%
+  select(K, exclusivity, semantic_coherence) %>%
+  filter(K %in% c(5, 10, 15)) %>%
+  unnest() %>%
+  mutate(K = as.factor(K)) %>%
+  ggplot(aes(semantic_coherence, exclusivity, color = K)) +
+  geom_point(size = 2, alpha = 0.7) +
+  labs(x = "Semantic coherence",
+       y = "Exclusivity",
+       title = "Comparaison entre cohérence sémantique et exclusivité",
+       subtitle = "Le meilleur compromis semble être 10 ou 15")
+
+# Par facilité d'analyse, vu que la solution à 10 et 15 sujets semble la plus pertinente, celle à 10 sujets a été retenue
+
+topic_model <- k_result %>% 
+  filter(K == 10) %>% 
+  pull(topic_model) %>% 
+  .[[1]]
+
+# On peut ensuite évaluer le bêta, qui correspond à la probabilité qu'un mot se trouve dans un sujet particulier
+
+td_beta <- tidy(topic_model)
+
+# Ainsi que gamma, qui correspond à la probabilité qu'un tweet appartienne à un sujet
+
+td_gamma <- tidy(topic_model, matrix = "gamma",
+                 document_names = rownames(tidy_sparse))
+
+# On combine ensuite les deux pour visualiser les sujets les plus présents et les mots qui ont le plus de chance
+# d'en faire partie
+
+top_terms <- td_beta %>%
+  arrange(beta) %>%
+  group_by(topic) %>%
+  top_n(7, beta) %>%
+  arrange(-beta) %>%
+  select(topic, term) %>%
+  summarise(terms = list(term)) %>%
+  mutate(terms = map(terms, paste, collapse = ", ")) %>% 
+  unnest(cols = c(terms))
+
+gamma_terms <- td_gamma %>%
+  group_by(topic) %>%
+  summarise(gamma = mean(gamma)) %>%
+  arrange(desc(gamma)) %>%
+  left_join(top_terms, by = "topic") %>%
+  mutate(topic = paste0("Topic ", topic),
+         topic = reorder(topic, gamma))
+
+gamma_terms %>%
+  top_n(20, gamma) %>%
+  ggplot(aes(topic, gamma, label = terms, fill = topic)) +
+  geom_col(show.legend = FALSE) +
+  geom_text(hjust = 0, nudge_y = 0.0005, size = 3,
+            family = "IBMPlexSans") +
+  coord_flip()  +
+  scale_y_continuous(expand = c(0,0),
+                     limits = c(0, 0.2),
+                     labels = scales::percent_format()) +
+  theme_tufte(base_family = "IBMPlexSans", ticks = FALSE) +
+  theme(plot.title = element_text(size = 16,
+                                  family="IBMPlexSans-Bold"),
+        plot.subtitle = element_text(size = 13)) +
+  labs(x = NULL, y = expression(gamma),
+       title = "Sujets, par prévalence, dans le corpus de tweets",
+       subtitle = "Accompagnés des mots principaux dans chaque sujet")
